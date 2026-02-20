242f5ee365cf70ee2d6c0262d862b4708f4beb88
kgrizz-git <kevin.grizzard@gmail.com>
2026-02-20 04:11:53 -0500

    Fix asset paths for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,8 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
   base: '/Seton/',
+  publicDir: 'assets',
   build: {
     outDir: 'dist',
-    assetsDir: 'assets',
   },
 });
